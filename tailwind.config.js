105be93c7162cc5b334fc5aafe10c4fbe5360095
Babszz <naija4nath@gmail.com>
2025-07-03 00:11:58 +0100

    some more tw config

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,18 @@ module.exports = {
   content: [],
   theme: {
     extend: {
+      colors: {
+        dark: "#181D27",
+        zana: {
+          grey: {
+            100: "#F5F5F5",
+            200: "#D5D7DA",
+            300: "#E9EAEB",
+            400: "#A4A7AE",
+          },
+          primary: { normal: "#255A5A" },
+        },
+      },
       fontFamily: {
         primary: ["var(--primary-font)"],
       },
