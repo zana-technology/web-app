15bc77b5d764d1c19c6898ec4dca3e2e744a9b70
Babszz <naija4nath@gmail.com>
2025-07-05 11:18:44 +0100

    create first services

---
+++ src/libs/api/routes/api-routes.ts
@@ -0,0 +1,15 @@
+const serviceRoot = {
+  auth: "v1/auth",
+  profile: "v1/profile",
+};
+
+export const apiRoutes = {
+  auth: {
+    login: `${serviceRoot.auth}/signin`,
+    signup: `${serviceRoot.auth}/signup`,
+    verifyEmail: `${serviceRoot.auth}/verify-email`,
+    resendVerificationEmail: `${serviceRoot.auth}/resend-verification-email`,
+    refreshToken: `${serviceRoot.auth}/refresh-token`,
+    logout: `${serviceRoot.auth}/logout`,
+  },
+};
---
+++ src/libs/api/services/auth.service.ts
@@ -0,0 +1,33 @@
+import { LoginFormValues, SignupFormValues } from "@/types";
+import { apiRequest } from "../config";
+import { apiRoutes } from "../routes";
+
+const login = async (payload: LoginFormValues) => {
+  return await apiRequest<LoginFormValues>({
+    url: apiRoutes.auth.login,
+    method: "post",
+    payload,
+  });
+};
+
+const signup = async (payload: SignupFormValues) => {
+  return await apiRequest<SignupFormValues>({
+    url: apiRoutes.auth.signup,
+    method: "post",
+    payload,
+  });
+};
+
+const verifyEmail = async (payload: SignupFormValues) => {
+  return await apiRequest<SignupFormValues>({
+    url: apiRoutes.auth.verifyEmail,
+    method: "post",
+    payload,
+  });
+};
+
+export const authService = {
+  login,
+  signup,
+  verifyEmail,
+};
